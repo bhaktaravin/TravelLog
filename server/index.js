e8dcada897e96f7f6b3e93a590967732897468f0
bhaktaravin <ravin.bhakta@gmail.com>
2022-10-05 10:01:39 -0700

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ app.use(cors());
 
 app.use(express.json()); 
 
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 
 // Connect to mongoose
 mongoose
